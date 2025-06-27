9660205997062a092b2dbe6e74194b24b9fd2133
Jimin0304 <pjws0304@naver.com>
2025-06-27 11:26:48 +0900

    [⚙️CHORE] yml에 swagger 설정 추가 및 application 이름 변경

--- src/main/java/com/kftc/KftcApplication.java
+++ src/main/java/com/kftc/KftcApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OnecarApplication {
+public class KftcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OnecarApplication.class, args);
+		SpringApplication.run(KftcApplication.class, args);
 	}
 
 }
